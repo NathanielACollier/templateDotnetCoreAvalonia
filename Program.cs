944f2aad9520d024dbe033b4e7abc59844390908
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-09-02 09:36:46 -0500

    Setup with dotnet core new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace avalonia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
